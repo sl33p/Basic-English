eea60768815ef6a1413188c5b5f6bb31740157c8
Nicklas Persson <n@XI.local>
2012-11-26 22:23:13 +0000

    Rails bootstrap
    Signed-off-by: Nicklas Persson <n@XI.local>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicEnglish::Application.config.secret_token = 'cbf1effe40aed91afd7752f662784148101673a4c75f56672df167de61a4d5ee70c01f841b21b851c8b51112afee7923d84f0623d767a249520f10409321666c'
